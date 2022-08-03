8f0c0b5d73689f8b2a73d9986985346d1e7819dc
sammulllx <2339576844@qq.com>
2022-08-03 22:18:57 +0800

    第一次提交

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+print("Current python version:")
+print(sys.version)
+print()
+print("Current python path:")
+print(sys.executable)
+print("dddd")
+
+a=1
+print(a)
+b =2
+c = a+b
+print(c)
+name = input("Please input your name: ")
+print("Hello, " + name + "!")
